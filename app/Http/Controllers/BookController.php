3d81ceda4d1bea9df90fd3da57403f2033b7f5bf
JoshuaDwiJulianto <mu56529@gmail.com>
2023-11-30 21:00:27 +0700

    Pertemuan 7 Dan 8

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Book;
 use App\Models\Bookshelf;
-
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\BooksExport;
+use App\Imports\BooksImport;
+use PDF;
 
 class BookController extends Controller
 {
@@ -101,6 +104,30 @@ class BookController extends Controller
         $book->delete();
         $notification = array(
             'message' => 'Data buku berhasil dihapus',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('book')->with($notification);
+    }
+    public function print()
+    {
+        $books = Book::all();
+        $pdf = PDF::loadview('books.print', ['books' => $books]);
+        return $pdf->download('data_buku.pdf');
+    }
+    public function export()
+    {
+        return Excel::download(new BooksExport, 'books.xlsx');
+    }
+    public function import(Request $req)
+    {
+        $req->validate([
+            'file' => 'required|max:10000|mimes:xlsx,xls',
+        ]);
+
+        Excel::import(new BooksImport, $req->file('file'));
+
+        $notification = array(
+            'message' => 'Import data berhasil dilakukan',
             'alert-type' => 'success'
         );
         return redirect()->route('book')->with($notification);
